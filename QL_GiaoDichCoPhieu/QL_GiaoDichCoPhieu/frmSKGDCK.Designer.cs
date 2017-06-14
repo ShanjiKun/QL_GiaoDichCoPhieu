696d9c426e111a7a706de8e0bbef7c4bb1d317e7
Tuan <hstrongtuan@gmail.com>
2017-06-14 18:43:30 +0700

    -Tra cứu số dư tiền, số dư cổ phiếu, sao kê giao dịch, sao kê lệnh đặt, tạo tài khoản NDT -còn 1 số lỗi chưa fix

---
+++ QL_GiaoDichCoPhieu/QL_GiaoDichCoPhieu/frmSKGDCK.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace QL_GiaoDichCoPhieu
+{
+    partial class frmSKGDCK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sao Kê Giao Dịch Chứng Khoáng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã cổ phiếu";
+            // 
+            // btnSK
+            // 
+            this.btnSK.Location = new System.Drawing.Point(282, 137);
+            this.btnSK.Name = "btnSK";
+            this.btnSK.Size = new System.Drawing.Size(75, 23);
+            this.btnSK.TabIndex = 3;
+            this.btnSK.Text = "Sao kê";
+            this.btnSK.UseVisualStyleBackColor = true;
+            this.btnSK.Click += new System.EventHandler(this.btnSK_Click);
+            // 
+            // frmSKGDCK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 188);
+            this.Controls.Add(this.btnSK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmSKGDCK";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmSKGDCK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSK;
+    }
+}
