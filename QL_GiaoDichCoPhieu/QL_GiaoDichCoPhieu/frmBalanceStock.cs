cfe422cc82c80e79897b0b51bf9c68ec0c679c7c
Tuan <hstrongtuan@gmail.com>
2017-06-15 15:37:01 +0700

    h

--- QL_GiaoDichCoPhieu/QL_GiaoDichCoPhieu/frmBalanceStock.cs
+++ QL_GiaoDichCoPhieu/QL_GiaoDichCoPhieu/frmBalanceStock.cs
@@ -19,11 +19,11 @@ namespace QL_GiaoDichCoPhieu
 
         private void frmBalanceStock_Load(object sender, EventArgs e)
         {
-            string sql = "select SH.MaCP, SH.SoLuong, CP.TenCty from SOHUU as SH, COPHIEU as CP where SH.MaNDT = 111111";
-            DataTable dtBalanceStock = Connection.getData(sql);
-            BindingSource bs = new BindingSource();
-            bs.DataSource = dtBalanceStock;
-            gctBalanceStock.DataSource = bs;
+            //string sql = "select SH.MaCP, SH.SoLuong, CP.TenCty from SOHUU as SH, COPHIEU as CP where SH.MaNDT = 111111";
+            //DataTable dtBalanceStock = Connection.getData(sql);
+            //BindingSource bs = new BindingSource();
+            //bs.DataSource = dtBalanceStock;
+            //gctBalanceStock.DataSource = bs;
         }
     }
 }
